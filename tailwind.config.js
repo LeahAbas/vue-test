c5fb0bbed79964c5568f5937bac43d5e601ee761
Leah <leahobot@gmail.com>
2023-09-24 17:40:31 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      "primary": "#00668A",
+      "secondary": "#004E71",
+    },
+    fontFamily: {
+      
+    }
+  },
+  plugins: [],
+}
+
